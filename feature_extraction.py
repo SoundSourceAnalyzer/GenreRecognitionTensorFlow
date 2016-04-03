eaab59ea514bb9731a4df503fac291933c2fec95
Pawel Cejrowski <pcejrowski@gmail.com>
2016-04-03 22:29:29 +0200

    Wyciaganie obecnie potrzebnych featuerow do plikow CSV

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,33 +1,37 @@
 # W zwiazku z tym, ze narazie nie mamy wlasnego wyciagania feature'ow uzywamy do tego YAAFE
-# Moze moglibysmy uzyc tego rowniez potem
+# Moze moglibysmy uzyc tego rowniez potem zamiast pisac cos swojego...
+
 # TODO: owinac calosc w docker razem z TensorFlowem
+# TODO: wciagnac skrypt dataSetPreparation tak, zeby rowniec pobieral dane
 from __future__ import print_function
-import numpy as np
 import os
-import sys
 
 from yaafelib import *
-def getFeatures(audiofile):
-    fp = FeaturePlan(sample_rate=22050)
-    fp.addFeature('mfcc: MFCC blockSize=512 stepSize=256')
-    fp.addFeature('mfcc_d1: MFCC blockSize=512 stepSize=256 > Derivate DOrder=1')
-    fp.addFeature('mfcc_d2: MFCC blockSize=512 stepSize=256 > Derivate DOrder=2')
+
+
+def get_features(audio_file):
+    print('Getting features from ' + audio_file)
+    fp = FeaturePlan(sample_rate=22050, normalize=1)  # sample rate of the .wav files TODO:check if only this
+    # Features that seems to be most often used, so they are good to start with.
+    fp.addFeature('mfcc: MFCC')
+    fp.addFeature('zcr: ZCR')
+    fp.addFeature('spectral_shape: SpectralShapeStatistics')
+    fp.addFeature('lsf: LSF')
+    fp.addFeature('magnitudeSpectrum: MagnitudeSpectrum')
     df = fp.getDataFlow()
     engine = Engine()
     engine.load(df)
     afp = AudioFileProcessor()
-    afp.processFile(engine,audiofile)
-    feats = engine.readAllOutputs()
-    print(feats)
-    afp.setOutputFormat('csv','output',{'Precision':'8'})
-    afp.processFile(engine,audiofile)
+    afp.setOutputFormat('csv', 'output', {'Precision': '8', 'Metadata': 'False'})
+    afp.processFile(engine, audio_file)
+    engine.flush()
 
-getFeatures('/data/team-project/genres/rock/rock.00050.wav')
 
-# dataset = '/data/team-project/genres/'
-# for genre in os.listdir(dataset):
-#     for sample in os.listdir(dataset + genre):
-#         if sample.endswith('.wav'):
-#             absPath = dataset + genre + '/' + sample
-#             print('Working on ' + absPath)
-#             getFeatures(absPath)
+# get_features('/data/team-project/genres/rock/rock.00050.wav')
+
+dataset = '/data/team-project/genres/'
+for genre in os.listdir(dataset):
+    for sample in os.listdir(dataset + genre):
+        if sample.endswith('.wav'):
+            absPath = dataset + genre + '/' + sample
+            get_features(absPath)
