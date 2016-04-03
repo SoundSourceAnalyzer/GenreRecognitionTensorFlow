0dad77fb21454c6c2ce382118bd7c42bb02b5cec
Pawel Cejrowski <pcejrowski@gmail.com>
2016-04-03 12:25:07 +0200

    Dodanie warunku konwersji przy przygotowywaniu datasetu

--- dataSetPreparation.sh
+++ dataSetPreparation.sh
@@ -7,6 +7,10 @@ for i in `find . -name \*.au -print`
 do
     old_file="${i}"
     new_file="${old_file%.*}.wav"
-    echo "Converting ${old_file} to ${new_file}"
-    sox ${old_file} ${new_file}
+    if ! [ -s ${new_file} ] ; then
+        echo "Converting ${old_file} to ${new_file}"
+        sox ${old_file} ${new_file}
+    else
+        echo "Already converted ${old_file}"
+    fi
 done
