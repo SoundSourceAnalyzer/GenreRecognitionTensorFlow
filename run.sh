9dc95c176e451ace781af09dffb8394fbb53c0a5
humblehound <dejtabejz@gmail.com>
2016-10-23 00:18:43 +0200

    GZTAN dataset extraction and training

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 set -euvx
 
 curdir=`pwd`
-sudo docker run -p 8888:8888 -v ${curdir}:/notebooks -it --rm pcej/tensor-with-yaafe:latest           
+sudo docker run -p 8888:8888 -v ${curdir}:/notebooks -it --rm pcej/tensor-with-yaafe:latest
